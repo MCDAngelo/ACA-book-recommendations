eeb301e0f8f3682321f1e212c1daf117e287ae69
MCDAngelo <dangelo.maria@gmail.com>
2024-09-24 21:12:41 -0400

    Ignore modules

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+const years = [2019, 2020, 2021, 2022, 2023];
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {years: years});
 })
 
 app.listen(port, () => {
